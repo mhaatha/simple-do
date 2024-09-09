7b4da94305046f2840b98183af37000e50b67fa3
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-09 17:24:29 +0800

    Create connection to database

---
+++ internal/database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// Open connection to database
+	db, err := sql.Open("mysql", "root:"+dbPassword+"@tcp(localhost:3306)/simple_do?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setting db pooling
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
