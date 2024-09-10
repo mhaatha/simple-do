cd14ff9be2ed885d0d5464715af942b80a7ae3c6
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-10 20:05:22 +0800

    Create SQL table and an useless simple welcome format

---
+++ cmd/todo/format.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Menu() {
+	fmt.Println("Welcome to the CLI To-Do List App!")
+	fmt.Println("----------------------------------")
+	fmt.Println("Please choose an option:")
+	fmt.Println("1. Add a Task")
+	fmt.Println("2. View Tasks")
+	fmt.Println("3. Mark Task as Done")
+	fmt.Println("4. Delete a Task")
+	fmt.Println("5. Exit")
+	fmt.Println("----------------------------------")
+	fmt.Println("Enter your choice:")
+}
