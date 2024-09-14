a0d03ec6a41ceaad52970dfc06e8ad830da82036
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-14 19:43:37 +0800

    Cobra versionCmd

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const appVersion = "1.0.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Simple-Do",
+	Long:  `All software has versions. This is Simple-Do's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Simple-Do CLI v%s\n", appVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
