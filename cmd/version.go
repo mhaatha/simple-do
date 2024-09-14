14ceb58d2a20ff5e4df2e3e841004fc2157a46d2
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-14 21:36:18 +0800

    Fix typos

--- cmd/version.go
+++ cmd/version.go
@@ -10,8 +10,8 @@ const appVersion = "1.0.0"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Simple-Do",
-	Long:  `All software has versions. This is Simple-Do's`,
+	Short: "Print the version number of Simple-Do.",
+	Long:  `All software has versions. This is Simple-Do's.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Simple-Do CLI v%s\n", appVersion)
 	},
