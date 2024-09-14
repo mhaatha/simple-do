b0cf272fe335e1ef899e78d447b5b9e957f982fb
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-14 19:43:20 +0800

    Cobra rootCmd

---
+++ cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "simple-do",
+	Short: "Simple-Do is a basic CLI tool to manage your to-do tasks.",
+	Long:  `Simple-Do is a command-line tool for managing tasks. You can add, remove, and list tasks easily.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- cmd/todo/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	menuChoice := Menu()
-	fmt.Println(menuChoice)
-}
